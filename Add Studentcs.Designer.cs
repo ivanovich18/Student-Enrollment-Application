0dc3077283db65be9f5d87e96c8eedaace82ee00
Ivan Suralta <ivan.suralta@ctu.edu.ph>
2023-04-24 12:06:41 +0800

    Added the student form page and its functionalities

---
+++ Add Studentcs.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Login_Form
+{
+    partial class Add_Studentcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(171, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Student Number:";
+            // 
+            // label2
+            // 
+            label2.Dock = DockStyle.Top;
+            label2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(0, 0);
+            label2.Margin = new Padding(8, 0, 8, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(1044, 83);
+            label2.TabIndex = 3;
+            label2.Text = "Add Student Form";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(559, 304);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 32);
+            label3.TabIndex = 4;
+            label3.Text = "Student ID:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(12, 276);
+            label4.Name = "label4";
+            label4.Size = new Size(134, 32);
+            label4.TabIndex = 5;
+            label4.Text = "First Name:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(12, 229);
+            label5.Name = "label5";
+            label5.Size = new Size(131, 32);
+            label5.TabIndex = 6;
+            label5.Text = "Last Name:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(12, 407);
+            label6.Name = "label6";
+            label6.Size = new Size(116, 32);
+            label6.TabIndex = 7;
+            label6.Text = "Birthdate:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(12, 449);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 32);
+            label7.TabIndex = 8;
+            label7.Text = "Sex:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(619, 96);
+            label8.Name = "label8";
+            label8.Size = new Size(225, 32);
+            label8.TabIndex = 9;
+            label8.Text = "Permanent Address:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(619, 142);
+            label9.Name = "label9";
+            label9.Size = new Size(147, 32);
+            label9.TabIndex = 10;
+            label9.Text = "Department:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(619, 184);
+            label10.Name = "label10";
+            label10.Size = new Size(160, 32);
+            label10.TabIndex = 11;
+            label10.Text = "Student Type:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(12, 184);
+            label11.Name = "label11";
+            label11.Size = new Size(173, 32);
+            label11.TabIndex = 12;
+            label11.Text = "Academic Year:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(12, 318);
+            label12.Name = "label12";
+            label12.Size = new Size(165, 32);
+            label12.TabIndex = 13;
+            label12.Text = "Middle Name:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label13.Location = new Point(12, 365);
+            label13.Name = "label13";
+            label13.Size = new Size(192, 32);
+            label13.TabIndex = 14;
+            label13.Text = "Extension Name:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.Location = new Point(12, 494);
+            label14.Name = "label14";
+            label14.Size = new Size(61, 32);
+            label14.TabIndex = 15;
+            label14.Text = "Age:";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label15.Location = new Point(12, 536);
+            label15.Name = "label15";
+            label15.Size = new Size(160, 32);
+            label15.TabIndex = 16;
+            label15.Text = "Place of Birth:";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label16.Location = new Point(12, 582);
+            label16.Name = "label16";
+            label16.Size = new Size(190, 32);
+            label16.TabIndex = 17;
+            label16.Text = "Current Address:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(44, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(257, 35);
+            textBox1.TabIndex = 18;
+            // 
+            // Add_Studentcs
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1044, 623);
+            Controls.Add(textBox1);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Add_Studentcs";
+            Text = "Add Student";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private TextBox textBox1;
+    }
+}
---
+++ Add Studentcs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class Add_Studentcs : Form
+    {
+        public Add_Studentcs()
+        {
+            InitializeComponent();
+        }
+    }
+}
