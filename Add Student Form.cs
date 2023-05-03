95281d5e860b01e36c8db67fa3fd153f4c4633ef
Ivan Suralta <ivan.suralta@ctu.edu.ph>
2023-05-04 00:50:26 +0800

    Updated the add student form of the application.

--- Add Student Form.cs
+++ Add Student Form.cs
@@ -10,6 +10,13 @@ using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using MySql.Data.MySqlClient;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using Microsoft.VisualBasic.Devices;
+using Microsoft.VisualBasic;
+using static Bunifu.UI.WinForms.Helpers.Transitions.Transition;
+using static System.Formats.Asn1.AsnWriter;
+using System.Runtime.Intrinsics.X86;
+using System.Reflection.PortableExecutable;
+using System.Web.Services.Description;
 
 namespace Login_Form
 {
@@ -127,6 +134,7 @@ namespace Login_Form
             AcademicYearCmbBox.SelectedIndex = 0;
             StudentTypeCmbBox.SelectedIndex = 0;
             DepartmentCmbBox.SelectedIndex = 0;
+            ProgramCmbBox.Items.Insert(0, "Program");
             ProgramCmbBox.SelectedIndex = 0;
         }
 
@@ -212,6 +220,110 @@ namespace Login_Form
             ProgramCmbBox.ValueMember = "Value";
 
             StudentIDLbl.ForeColor = Color.White;
+
+            DepartmentCmbBox.Items.Add("(CAS) College of Arts and Sciences");
+            DepartmentCmbBox.Items.Add("(CCICT) College of Computer, Information and Communications Technology");
+            DepartmentCmbBox.Items.Add("(COEd) College of Education");
+            DepartmentCmbBox.Items.Add("(COE) College of Engineering");
+            DepartmentCmbBox.Items.Add("(CME) College of Management and Entrepreneurship");
+            DepartmentCmbBox.Items.Add("(COT) College of Technology");
+        }
+
+        private void DepartmentCmbBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ProgramCmbBox.Items.Clear();
+
+            switch (DepartmentCmbBox.SelectedIndex)
+            {
+                case 0: // Default
+                    break;
+                case 1: // CAS
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BAEL-ELSD) Bachelor of Arts in English Language - English Language Studies as a Discipline");
+                    ProgramCmbBox.Items.Add("(BAEL-ELAP) Bachelor of Arts in English Language - English Language Across Professions");
+                    ProgramCmbBox.Items.Add("(BAL-LCS) Bachelor of Arts in Literature - Literary Cultural Studies");
+                    ProgramCmbBox.Items.Add("(BAL-LAP) Bachelor of Arts in Literature - Literature Across Professions");
+                    ProgramCmbBox.Items.Add("(BS Bio-Ecology) Bachelor of Science in Biology - Ecology");
+                    ProgramCmbBox.Items.Add("(BS Dev Com) Bachelor of Science in Development Communication");
+                    ProgramCmbBox.Items.Add("(BAF) Batsilyer ng Sining sa Filipino");
+                    ProgramCmbBox.Items.Add("(BS Math) Bachelor of Science in Mathematics");
+                    ProgramCmbBox.Items.Add("(BS Psych) Bachelor of Science in Psychology");
+                    ProgramCmbBox.Items.Add("(BS Stat) Bachelor of Science in Statistics");
+                    ProgramCmbBox.Items.Add("(BSN) Bachelor of Science in Nursing at Cebu City Medical Center - College of Nursing");
+                    break;
+                case 2: // CCICT
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BSIT)Bachelor of Science in Information Technology");
+                    ProgramCmbBox.Items.Add("(BSIS) Bachelor of Science in Information Systems");
+                    ProgramCmbBox.Items.Add("(BIT-CT) Bachelor of Industrial Technology - Computer Technology");
+                    break;
+                case 3: // COEd
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BEEd) Bachelor of Elementary Education");
+                    ProgramCmbBox.Items.Add("(BSEd-Mathematics) Bachelor of Secondary Education - Mathematics");
+                    ProgramCmbBox.Items.Add("(BSEd-Science) Bachelor of Secondary Education - Science");
+                    ProgramCmbBox.Items.Add("(BSEd-Filipino) Bachelor of Secondary Education- Filipino");
+                    ProgramCmbBox.Items.Add("(BSEd-Social Studies) Bachelor of Secondary Education - Social Studies");
+                    ProgramCmbBox.Items.Add("(BTLEd-Home Economics) Bachelor of Technology and Livelihood Education - Home Economics");
+                    ProgramCmbBox.Items.Add("(BTLEd-Information and Communication Technology) Bachelor of Technology and Livelihood Education - Information and Communication Technology");
+                    ProgramCmbBox.Items.Add("(BTLEd-Industrial Arts) Bachelor of Technology and Livelihood Education - Industrial Arts");
+                    ProgramCmbBox.Items.Add("(BECEd) Bachelor of Early Childhood Education");
+                    ProgramCmbBox.Items.Add("(BSNEd) Bachelor of Special Needs Education");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Automotive Technology) Bachelor of Technical-Vocational Teacher Education - Automotive Technology");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Civil and Construction Technology) Bachelor of Technical-Vocational Teacher Education - Civil and Construction Technology");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Drafting Technology) Bachelor of Technical-Vocational Teacher Education - Drafting Technology");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Electrical Technology) Bachelor of Technical-Vocational Teacher Education - Electrical Technology");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Electronics Technology) Bachelor of Technical-Vocational Teacher Education - Electronics Technology");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Garments and Fashion Design) Bachelor of Technical-Vocational Teacher Education - Garments and Fashion Design");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Food and Service Management) Bachelor of Technical-Vocational Teacher Education - Food and Service Management");
+                    ProgramCmbBox.Items.Add("(BTVTEd-Welding and Fabrication Technology) Bachelor of Technical-Vocational Teacher Education - Welding and Fabrication Technology");
+                    ProgramCmbBox.Items.Add("(CT-Computer Technology) Certificate of Technology - Computer Technology (for deaf student-applicants only)");
+                    ProgramCmbBox.Items.Add("(CT-Food Technology) Certificate of Technology - Food Technology (for deaf student-applicants only)");
+                    break;
+                case 4: // COE
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BSAsE)Bachelor of Science in Aerospace Engineering");
+                    ProgramCmbBox.Items.Add("(BSCE) Bachelor of Science in Civil Engineering");
+                    ProgramCmbBox.Items.Add("(BSCpE) Bachelor of Science in Computer Engineering");
+                    ProgramCmbBox.Items.Add("(BSECE) Bachelor of Science in Electronics Engineering");
+                    ProgramCmbBox.Items.Add("(BSEE) Bachelor of Science in Electrical Engineering");
+                    ProgramCmbBox.Items.Add("(BSIE) Bachelor of Science in Industrial Engineering");
+                    ProgramCmbBox.Items.Add("(BSME) Bachelor of Science in Mechanical Engineering");
+                    break;
+                case 5: // CME
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BSBA-MM) Bachelor of Science in Business Administration - Marketing Management");
+                    ProgramCmbBox.Items.Add("(BSHM) Bachelor of Science in Hospitality Management");
+                    ProgramCmbBox.Items.Add("(BPA) Bachelor of Public Administration");
+                    ProgramCmbBox.Items.Add("(BSTM) Bachelor of Science in Tourism Management");
+                    break;
+                case 6: // COT
+                    ProgramCmbBox.Items.Insert(0, "Program");
+                    ProgramCmbBox.SelectedIndex = 0;
+                    ProgramCmbBox.Items.Add("(BSGD)Bachelor of Science in Graphics and Design");
+                    ProgramCmbBox.Items.Add("(BSMx) Bachelor of Science in Mechatronics");
+                    ProgramCmbBox.Items.Add("(BSTechM) Bachelor of Science in Technology Management");
+                    ProgramCmbBox.Items.Add("(BIT-Automotive Technology) Bachelor of Industrial Technology - Automotive Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Civil Technology) Bachelor of Industrial Technology - Civil Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Cosmetology) Bachelor of Industrial Technology - Cosmetology");
+                    ProgramCmbBox.Items.Add("(BIT-Drafting Technology) Bachelor of Industrial Technology - Drafting Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Electrical Technology) Bachelor of Industrial Technology - Electrical Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Electronics Technology) Bachelor of Industrial Technology - Electronics Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Furniture and Cabinetmaking Technology) Bachelor of Industrial Technology - Furniture and Cabinetmaking Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Food Preparation and Services Technology) Bachelor of Industrial Technology - Food Preparation and Services Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Garments Technology) Bachelor of Industrial Technology - Garments Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Interior Design Technology) Bachelor of Industrial Technology - Interior Design Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Machine Shop Technology) Bachelor of Industrial Technology - Machine Shop Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Power Plant Technology) Bachelor of Industrial Technology - Power Plant Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Refrigeration and Air Conditioning - Technology) Bachelor of Industrial Technology - Refrigeration and Air Conditioning Technology");
+                    ProgramCmbBox.Items.Add("(BIT-Welding and Fabrication Technology) Bachelor of Industrial Technology - Welding and Fabrication Technology");
+                    break;
+            }
         }
     }
 }
