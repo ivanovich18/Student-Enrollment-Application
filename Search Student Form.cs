7002e5de24b43ef70fdbbc34212832f95843032c
Ivan Suralta <ivan.suralta@ctu.edu.ph>
2023-05-14 22:06:24 +0800

    Updated the search student

--- Search Student Form.cs
+++ Search Student Form.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Login_Form
 {
@@ -18,10 +20,12 @@ namespace Login_Form
             InitializeComponent();
         }
 
-        private void RegisterBtn_Click(object sender, EventArgs e)
-        {
 
+        public string idNumber()
+        {
+            return StudentNumberTxtBox.Text;
         }
+        
 
         private void BackBtn_Click(object sender, EventArgs e)
         {
@@ -32,14 +36,37 @@ namespace Login_Form
 
         private void SearchBtn_Click(object sender, EventArgs e)
         {
-            // Get the ID number from the text box
-            string idNumber = StudentNumberTxtBox.Text;
 
-            // Set up a MySQL connection string
+            // Establish a connection to the MySQL database
             string connectionString = "server=localhost;user=root;password=;database=student_enrollment_application";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
 
-            // Create a MySQL connection
-            MySqlConnection connection = new MySqlConnection(connectionString);
+                // Define a query to retrieve a row from a table based on the student number
+                string query = "SELECT * FROM app_student_records WHERE student_id = @idNumber";
+                MySqlCommand command = new MySqlCommand(query, connection);
+
+                // Add a parameter to the query based on the value of the TextBox
+                string idNumber = StudentNumberTxtBox.Text;
+                command.Parameters.AddWithValue("@idNumber", idNumber);
+
+                // Execute the query and retrieve the row
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    // Convert the row into a string
+                    string rowAsString = string.Format("Last Name: {0} \nFirst Name: {1}",
+                        reader["last_name"], reader["first_name"]);
+
+                    // Display the string in a MessageBox
+                    MessageBox.Show(rowAsString);
+                }
+                reader.Close();
+            }
+            /*
+                // Create a MySQL connection
+                MySqlConnection connection = new MySqlConnection(connectionString);
 
             // Create a MySQL command to search for the ID number
             string query = "SELECT * FROM app_student_records WHERE student_id = @idNumber";
@@ -53,16 +80,48 @@ namespace Login_Form
             // Check if the ID number exists and display a message box if it does
             if (reader.Read())
             {
-                MessageBox.Show("ID number exists!");
+                DialogResult result = MessageBox.Show("Student ID number exists!", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result == DialogResult.Yes)
+                {
+                    //Student_Found student_Found = new Student_Found();
+                    //student_Found.ShowDialog();
+
+                    MySqlCommand cmd = new MySqlCommand("SELECT * FROM app_student_records WHERE student_id = @idNumber", connection);
+
+                    // string query = "SELECT * FROM app_student_records WHERE student_id = @idNumber";
+                    cmd.Parameters.AddWithValue("@idNumber", idNumber);
+
+                    // Close the reader and connection
+                    reader.Close();
+                    connection.Close();
+                    /*
+                    // execute command and retrieve count value as object
+                    object last_name = cmd.ExecuteScalar();
+                    // convert object to string and store in variable
+                    string ln = last_name.ToString();
+                    // close connection
+                    conn.Close();
+
+                    LastNameLbl.Text += " " + ln;
+                    // Execute the query and retrieve the row
+                    MySqlDataReader read = cmd.ExecuteReader();
+                    if (read.Read())
+                    {
+                        // Convert the row into a string
+                        string rowAsString = string.Format("{0}", reader["last_name"]);
+
+                        // Display the string in a MessageBox
+                        MessageBox.Show("Last Name: " + rowAsString);
+                    }
+                    read.Close();
+                }
             }
             else
             {
-                MessageBox.Show("ID number does not exist.");
+                MessageBox.Show("Student ID number does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                StudentNumberTxtBox.Clear();
             }
-
-            // Close the reader and connection
-            reader.Close();
-            connection.Close();
+            */
         }
     }
 }
---
+++ Student Found.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Login_Form
+{
+    partial class Student_Found
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LastNameLbl = new Label();
+            FirstNameLbl = new Label();
+            SuspendLayout();
+            // 
+            // LastNameLbl
+            // 
+            LastNameLbl.AutoSize = true;
+            LastNameLbl.Location = new Point(32, 46);
+            LastNameLbl.Name = "LastNameLbl";
+            LastNameLbl.Size = new Size(69, 15);
+            LastNameLbl.TabIndex = 0;
+            LastNameLbl.Text = "Last Name: ";
+            // 
+            // FirstNameLbl
+            // 
+            FirstNameLbl.AutoSize = true;
+            FirstNameLbl.Location = new Point(32, 79);
+            FirstNameLbl.Name = "FirstNameLbl";
+            FirstNameLbl.Size = new Size(67, 15);
+            FirstNameLbl.TabIndex = 1;
+            FirstNameLbl.Text = "First Name:";
+            // 
+            // Student_Found
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(679, 512);
+            Controls.Add(FirstNameLbl);
+            Controls.Add(LastNameLbl);
+            Name = "Student_Found";
+            Text = "Student_Found";
+            Load += Student_Found_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LastNameLbl;
+        private Label FirstNameLbl;
+    }
+}
