fa6c62807e52b40c0ed9ece9e214733f612cffdf
Adrin <Adrin@DESKTOP-AH38UQV>
2023-05-14 20:56:44 +0800

    add search student function

--- Search Student Form.cs
+++ Search Student Form.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,6 +28,41 @@ namespace Login_Form
             this.Hide();
             AdminDashboardForm mainForm = new AdminDashboardForm();
             mainForm.ShowDialog();
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            // Get the ID number from the text box
+            string idNumber = StudentNumberTxtBox.Text;
+
+            // Set up a MySQL connection string
+            string connectionString = "server=localhost;user=root;password=;database=student_enrollment_application";
+
+            // Create a MySQL connection
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            // Create a MySQL command to search for the ID number
+            string query = "SELECT * FROM app_student_records WHERE student_id = @idNumber";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@idNumber", idNumber);
+
+            // Open the connection and execute the command
+            connection.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+
+            // Check if the ID number exists and display a message box if it does
+            if (reader.Read())
+            {
+                MessageBox.Show("ID number exists!");
+            }
+            else
+            {
+                MessageBox.Show("ID number does not exist.");
+            }
+
+            // Close the reader and connection
+            reader.Close();
+            connection.Close();
         }
     }
 }
