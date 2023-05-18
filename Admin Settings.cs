daf2b86237b23bbc7ce98accc73973e3d9caab3f
Ivan Suralta <ivan.suralta@ctu.edu.ph>
2023-05-18 20:09:01 +0800

    Added comments to the admin settings

--- Admin Settings.cs
+++ Admin Settings.cs
@@ -4,29 +4,28 @@
     {
         public AdminSettingsForm()
         {
-            InitializeComponent();
-        }
-
-        private void SettingsBackBtn_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            AdminDashboardForm mainForm = new AdminDashboardForm();
-            mainForm.ShowDialog();
-        }
-
-        private void ChangeUsernameBtn_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            ChangeUsernameForm changeUsernameForm = new ChangeUsernameForm();
-            changeUsernameForm.ShowDialog();
+            InitializeComponent(); // Initializes the AdminSettingsForm.
         }
 
         private void ChangePasswordBtn_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            ChangePasswordLbl changePasswordForm = new ChangePasswordLbl();
-            changePasswordForm.ShowDialog();
+            this.Hide(); // Hides the current AdminSettingsForm.
+            ChangePasswordForm changePasswordForm = new ChangePasswordForm(); // Creates an instance of the ChangePasswordForm.
+            changePasswordForm.ShowDialog(); // Displays the changePasswordForm as a dialog.
+        }
 
+        private void ChangeUsernameBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Hides the current AdminSettingsForm.
+            ChangeUsernameForm changeUsernameForm = new ChangeUsernameForm(); // Creates an instance of the ChangeUsernameForm.
+            changeUsernameForm.ShowDialog(); // Displays the changeUsernameForm as a dialog.
+        }
+
+        private void SettingsBackBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Hides the current AdminSettingsForm.
+            AdminDashboardForm dashboardForm = new AdminDashboardForm(); // Creates an instance of the AdminDashboardForm.
+            dashboardForm.ShowDialog(); // Displays the dashboardForm as a dialog.
         }
     }
 }
